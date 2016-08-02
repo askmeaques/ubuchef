0d80693bebe48234134ed40e87607fa9d673ed18
amykruger <amy.kruger@yahoo.com>
2016-08-01 17:13:23 -0700

    second commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["redchef6"] = { "site_title" => "Amys website coming soon ha ha","port" => 80, "domain" => "redchef6" }
+default["apache"]["sites"]["redchef6.localdomain"] = {"site_title" => "Amys website coming soon ha ha", "port" => 80, "domain" => "redchef6.localdomain" }
